ae76c00d372f07f2a22be4169d1ce5dcf4ad8483
notcwall <conorseamus@gmail.com>
2016-08-06 15:59:40 -0500

    heroku

--- server.js
+++ server.js
@@ -20,4 +20,10 @@ app.set('view engine', 'handlebars');
 var routes = require('./controllers/burgers_controller.js');
 app.use('/', routes);
 
-app.listen(process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3000);
+
+models.sequelize.sync().then(function(){
+	var server = app.listen(app.get('port'), function(){
+		debug('Express server listening on port ' + server.address().port);
+	});
+});
