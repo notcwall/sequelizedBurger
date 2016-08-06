68d543c98bca4e408c13c2406f729d318d5b1919
notcwall <conorseamus@gmail.com>
2016-08-06 15:42:50 -0500

    heroku again

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
+var models = require('./models');
+var debug = require('debug')('express-example');
 
 var app = express();
 
@@ -18,4 +20,10 @@ app.set('view engine', 'handlebars');
 var routes = require('./controllers/burgers_controller.js');
 app.use('/', routes);
 
-app.listen(3000);
+app.set('port', process.env.PORT || 3000);
+
+models.sequelize.sync().then(function(){
+	var server = app.listen(app.get('port'), function(){
+		debug('Express server listening on port ' + server.address().port);
+	});
+});
